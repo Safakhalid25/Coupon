cf819fdd50ebd3e3cf7887ef7ad74cf7ff049b16
Safa Khalid <safakhalid180@gmail.comgit configgit config --global user.email safakhalid180@gmail.com>
2024-12-02 15:58:31 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import UserRoutes from "./UserRoutes/index";
+
+function App() {
+  return (
+
+    <UserRoutes/>
+  );
+}
+
+export default App;
